2c357cae5df9b1e9ca5bd69a8badb0c203ebe860
SpataruLoredana <loredana.spataru@softvision.ro>
2019-10-13 02:33:10 +0300

    Create dataset and update app

---
+++ src/dataset.ts
@@ -0,0 +1,35 @@
+export const dataset = {
+  '0001': {
+    title: 'Color Transformation',
+    description: 'Lorem ipsum dolor sit amet',
+    id: '0001',
+    table: {
+      'Anthracite': 'Dark Grey',
+      'Midnight Black': 'Black',
+      'Mystic Silver': 'Silver',
+      'Blush Pink': 'Pink'
+    }
+  },
+  '0002': {
+    title: 'Size Transformation',
+    description: 'Lorem ipsum dolor sit amet',
+    id: '0001',
+    table: {
+      's': 'Small',
+      'm': 'Medium',
+      'l': 'Large',
+      'xl': 'Extra Large'
+    }
+  },
+  '0003': {
+    title: 'Country Transformation',
+    description: 'Lorem ipsum dolor sit amet',
+    id: '0001',
+    table: {
+      'United States': 'USA',
+      'United Kingdom': 'UK',
+      'France': 'FRA',
+      'Germany': 'GER'
+    }
+  }
+};
