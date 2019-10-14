31768e57fc769ee9798777646215dd45385618e2
SpataruLoredana <loredana.spataru@softvision.ro>
2019-10-14 06:59:15 +0300

    Updates

---
+++ precache-manifest.59239f2a0c869dd60642f3c5ae63fd86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd7b4370f6349abb4b9165288e906df9",
+    "url": "/dictionaries-app/index.html"
+  },
+  {
+    "revision": "33f138eca4d1916545d1",
+    "url": "/dictionaries-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "925a397dc54babe15b17",
+    "url": "/dictionaries-app/static/css/main.71d2a103.chunk.css"
+  },
+  {
+    "revision": "33f138eca4d1916545d1",
+    "url": "/dictionaries-app/static/js/2.e20250cc.chunk.js"
+  },
+  {
+    "revision": "925a397dc54babe15b17",
+    "url": "/dictionaries-app/static/js/main.5313fb65.chunk.js"
+  },
+  {
+    "revision": "95d8820e2b3553b48e89",
+    "url": "/dictionaries-app/static/js/runtime-main.aaa23247.js"
+  }
+]);
--- src/constants.ts
+++
@@ -1,5 +0,0 @@
-export const VALIDATIONS = {
-  EMPTY: 'Row cannot have empty fields. Please fill in the data.',
-  CLONE: 'Dictionary cannot have duplicate rows. Please remove one of them.',
-  FORK: `Duplicate value on 'From' field. Please remove marked row or update field.`
-};
